57a944c439fce98c33108d1499d6647b90a94ee6
yan <xueyan@ont.io>
2023-11-23 22:39:03 +0800

    update: 增加批量操作

---
+++ Example/Source/View Controllers/Settings/DestinationView.swift
@@ -0,0 +1,19 @@
+//
+//  DestinationView.swift
+//  nRF Mesh
+//
+//  Created by yan on 2023/11/23.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+struct DestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationView()
+}
---
+++ Example/Source/View Controllers/Settings/GroupsSendView.swift
@@ -0,0 +1,19 @@
+//
+//  GroupsSendView.swift
+//  nRF Mesh
+//
+//  Created by yan on 2023/11/23.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+struct GroupsSendView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupsSendView()
+}
